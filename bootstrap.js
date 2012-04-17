82b29bc86aca5baa1ed0ed219ad1ad4fb8247da1
Nate Irwin <nate@nateirwin.net>
2012-04-17 16:42:30 -0600

    Quick fix for detecting jQuery noConflict mode.

--- bootstrap.js
+++ bootstrap.js
@@ -332,7 +332,12 @@ NPMap.utils.injectCss(NPMap.config.server + '/npmap/' + NPMap.version + '/resour
          * Requires: jQuery 1.2.3+
          */
         ;(function(a){a.fn.extend({actual:function(b,k){var c,d,h,g,f,j,e,i;if(!this[b]){throw'$.actual => The jQuery method "'+b+'" you called does not exist';}h=a.extend({absolute:false,clone:false,includeMargin:undefined},k);d=this;if(h.clone===true){e=function(){d=d.filter(":first").clone().css({position:"absolute",top:-1000}).appendTo("body");};i=function(){d.remove();};}else{e=function(){c=d.parents().andSelf().filter(":hidden");g=h.absolute===true?{position:"absolute",visibility:"hidden",display:"block"}:{visibility:"hidden",display:"block"};f=[];c.each(function(){var m={},l;for(l in g){m[l]=this.style[l];this.style[l]=g[l];}f.push(m);});};i=function(){c.each(function(m){var n=f[m],l;for(l in g){this.style[l]=n[l];}});};}e();j=d[b](h.includeMargin);i();return j;}});})(jQuery);
-
+		
+		// TODO: You need to add support for operating in noConflict mode.
+		if (typeof $ === 'undefined') {
+		  $ = jQuery;
+		}
+		
         $(document).ready(function() {
           loadMapping();
         });
